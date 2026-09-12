e9462be9b7d402417e3627e1b5ad956e9354b3cc
WilksoPedro <wpsilva.dev@gmail.com>
2026-09-12 20:49:53 -0300

    Adicionado a estrutura inicial da landing 'Em construção' com asset

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 function Home() {
-  return <h1>YongTech</h1>;
+  return (
+    <>
+      <main>
+        <header>
+          <h1>YongTech</h1>
+        </header>
+        <p>Welcome to YongTech page in construction</p>
+        <section>
+          <div>
+            <img
+              src="../images/landing/yongtech-construction-hero.png"
+              alt="Hero image"
+            />
+          </div>
+          <div>
+            <p>
+              Transformando <strong>imaginação</strong> em experiências{" "}
+              <strong>vivas</strong>. Um laboratório onde código, mundos, jogos
+              e ideias começam a ganhar forma.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer>© 2026 Yongtech. All rights reserved.</footer>
+    </>
+  );
 }
 
 export default Home;
