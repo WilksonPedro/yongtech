0880e5a1c50a5c0be911c650edc7fc98372e4047
WilksoPedro <wpsilva.dev@gmail.com>
2026-09-12 16:15:16 -0300

    Add files '.env' an 'database.js'

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
   response.status(200).json({
     chave:
       "Bem vindo a minha Oficina, no momento minha pagina ainda esta em construção, obrigado por visitar!",
